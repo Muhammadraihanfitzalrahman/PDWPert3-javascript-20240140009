09b83774ea9d922f3d12c8c20eddbc72e74f29e1
ayamtulebo19@gmail.com <Ayamtulebo19@gmail.com>
2026-03-11 15:20:10 +0700

    digunakan untuk memberi tahu browser bahwa dokumen menggunakan HTML5

---
+++ script.js
@@ -0,0 +1,23 @@
+//fungsi menampilkan tanggal hari ini
+function date() {
+
+    let tanggal = new Date();
+
+    document.getElementById("tanggal").innerHTML =
+        "Tanggal hari ini : " + tanggal;
+
+}
+
+//fungsi ketika tombol diklik
+function tampil() {
+
+    alert("Ini adalah button");
+
+}
+
+//fungsi alert perkenalan
+function show() {
+
+    alert("Halo, saya sedang belajar JavaScript!");
+
+}
